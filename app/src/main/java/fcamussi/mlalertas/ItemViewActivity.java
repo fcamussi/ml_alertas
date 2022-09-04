52b088174d1a46444d42ef9f0fa3366aee6b3681
Fernando Camussi <fcamussi@gmail.com>
2022-09-04 14:50:57 -0300

    ItemViewActivity ExecutorService thumbnail

--- app/src/main/java/fcamussi/mlalertas/ItemViewActivity.java
+++ app/src/main/java/fcamussi/mlalertas/ItemViewActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +17,18 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ItemViewActivity extends AppCompatActivity {
 
     private Button btnOpenInML;
     private String permalink;
+    private String thumbnailLink;
+    private Bitmap thumbnail;
     private ActivityResultLauncher<Intent> openInMLLauncher;
 
     @Override
@@ -41,11 +49,6 @@ public class ItemViewActivity extends AppCompatActivity {
         TextView tvDetails2 = findViewById(R.id.item_view_tv_details2);
         btnOpenInML = findViewById(R.id.item_view_btn_open_in_ml);
         tvTitle.setText(item.getTitle());
-        byte[] thumbnail = item.getThumbnail();
-        if (thumbnail != null) {
-            Bitmap thumbnailBitmap = BitmapFactory.decodeByteArray(thumbnail, 0, thumbnail.length);
-            ivThumbnail.setImageBitmap(thumbnailBitmap);
-        }
         String detail1 = String.format(Locale.US, "Provincia: %s", item.getState());
         String detail2 = String.format(Locale.US, "Precio: %s %s",
                 item.getCurrency(),
@@ -53,6 +56,30 @@ public class ItemViewActivity extends AppCompatActivity {
         tvDetails1.setText(detail1);
         tvDetails2.setText(detail2);
         permalink = item.getPermalink();
+        thumbnailLink = item.getThumbnailLink();
+        thumbnailLink = thumbnailLink.substring(0, thumbnailLink.length() - 5) + "H.jpg";
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InputStream in = new URL(thumbnailLink).openStream();
+                    thumbnail = BitmapFactory.decodeStream(in);
+                    in.close();
+                } catch (Exception e) {
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (thumbnail != null) {
+                            ivThumbnail.setImageBitmap(thumbnail);
+                        }
+                    }
+                });
+            }
+        });
 
         openInMLLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
