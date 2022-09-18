0a6765ec18ec0bfdcdc7467fbe94b3fed94f091a
Fernando Camussi <fcamussi@gmail.com>
2022-09-17 23:49:22 -0300

    words mayus

--- app/src/main/java/fcamussi/mlalertas/AddSearchActivity.java
+++ app/src/main/java/fcamussi/mlalertas/AddSearchActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -45,6 +46,7 @@ public class AddSearchActivity extends AppCompatActivity {
 
         DataBase dataBase = new DataBase(this);
         etWords = findViewById(R.id.add_search_et_words);
+        etWords.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
         spinnerSite = findViewById(R.id.add_search_sp_site);
         spinnerFrequency = findViewById(R.id.add_search_sp_frequency);
         ivWordsInfo = findViewById(R.id.add_search_iv_words_info);
